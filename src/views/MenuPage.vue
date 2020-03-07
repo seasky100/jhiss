16e6e1b75fb2a9aaf4f71842b344d58d2e30a922
wangxin2 <1130779739@qq.com>
2020-03-07 14:43:18 +0800

    project

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -173,13 +173,13 @@ export default {
     },
     handleSelect(key, keyPath) {
       this.activeMenu = key
-      console.log(key, keyPath);
+      console.log(key, keyPath)
     },
     handleClose(key, keyPath) {
-      console.log(key, keyPath);
+      console.log(key, keyPath)
     },
     switchClick(){
-      this.isCollapse = !this.isCollapse;
+      this.isCollapse = !this.isCollapse
     },
     enter(){
       this.flag = true
