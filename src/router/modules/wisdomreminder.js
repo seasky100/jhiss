9eee1d1f94974b15ede49c7c20ba61e78db974ee
wuxiaoqiang <wuxiaoqiang@greatmap.com.cn>
2020-04-27 10:22:29 +0800

    智慧监督路由调整，去掉无效文件

--- src/router/modules/wisdomreminder.js
+++ src/router/modules/wisdomreminder.js
@@ -1,145 +1,191 @@
 const wisdomReminderRouter = [
   {
-    title: '智慧提醒',
+    title: '智慧监督',
     path: '/wisdom-reminder',
-    name: 'WisdomReminder',
     icon: 'el-icon-s-opportunity',
     img: 'left_inspection@2x.png',
     component: () => import('@/views/Supervision'),
     children: [
       {
-        title: '智慧考勤',
-        icon: 'el-icon-menu',
+        title: '政工监督',
         img: 'float_attendance@2x.png',
-        name: 'WisdomAttendance',
-        path: "/AttendanceAnalysis",
-        component: () => import('@/views/Supervision'),
+        path: 'zhenggong',
+        component: { render: (h) => h('router-view')},
         children: [
+          {
+            title: '工作评价',
+            img: 'float_absence@2x.png',
+            icon: 'el-icon-menu',
+            name: 'EvaluationAnalysis',
+            path: '/evaluation-analysis',
+            component: () => import('@/views/WisdomReminder/EvaluationAnalysis'),
+          },
           {
             title: '智慧考勤分析',
             img: 'left_menu_sub_analysis.svg',
             name: 'AttendanceAnalysis',
             path: '/attendance-analysis',
-            component: () => import('@/views/WisdomReminder/WisdomAttendance/AttendanceAnalysis')
+            component: () => import('@/views/WisdomReminder/WisdomAttendance/AttendanceAnalysis'),
           },
           {
             title: '考勤预警管控',
             img: 'left_menu_sub_alarm.svg',
             name: 'AttendanceWarning',
             path: '/attendance-warning',
-            component: () => import('@/views/WisdomReminder/WisdomAttendance/AttendanceWarning')
-          }
-        ]
-      },
-      {
-        title: '出国（镜）分析',
-        icon: 'el-icon-menu',
-        img: 'float_departure@2x.png',
-        name: 'GoAbroad',
-        path: "/go-abroad",
-        component: () => import('@/views/WisdomReminder/GoAbroad')
-      },
-      {
-        title: '请销假分析',
-        icon: 'el-icon-menu',
-        img: 'float_absence@2x.png',
-        name: 'LeaveAnalysis',
-        path: '/leave-analysis',
-        component: () => import('@/views/Supervision'),
-        children: [
+            component: () => import('@/views/WisdomReminder/WisdomAttendance/AttendanceWarning'),
+          },
+          {
+            title: '请假加班',
+            icon: 'el-icon-menu',
+            img: 'left_ledger@2x.png',
+            name: 'WisdomAttendance',
+            path: "/leave-overtime",
+            component: () => import('@/views/IntegrityRisk/LeaveAndOvertime')
+          },
           {
             title: '请假情况分析',
             img: 'float_absence@2x.png',
             path: '/LeaveSituationAnalysis',
-            component: () => import('@/views/WisdomReminder/LeaveAnalysis/LeaveSituationAnalysis')
+            component: () => import('@/views/WisdomReminder/LeaveAnalysis/LeaveSituationAnalysis'),
           },
           {
             title: '审批记录分析',
             img: 'float_absence@2x.png',
             path: '/ApprovalRecordAnalysis',
-            component: () => import('@/views/WisdomReminder/LeaveAnalysis/ApprovalRecordAnalysis')
-          }
-        ]
+            component: () => import('@/views/WisdomReminder/LeaveAnalysis/ApprovalRecordAnalysis'),
+          },
+          {
+            title: '出国（镜）分析',
+            icon: 'el-icon-menu',
+            img: 'float_departure@2x.png',
+            name: 'GoAbroad',
+            path: '/go-abroad',
+            component: () => import('@/views/WisdomReminder/GoAbroad'),
+          },
+          {
+            title: '护照管理',
+            icon: 'el-icon-menu',
+            img: 'left_ledger@2x.png',
+            name: 'PassportManagement',
+            path: '/PassportManagement',
+            component: () => import('@/views/WisdomReminder/PassportManagement'),
+          },
+        ],
       },
       {
-        title: '智慧感知',
-        icon: 'el-icon-menu',
-        img: 'left_sensing@2x.png',
-        name: 'PerceptualWisdom',
-        path: "/perceptual-wisdom",
-        component: () => import('@/views/Supervision'),
+        title: '纪检监督',
+        img: 'float_attendance@2x.png',
+        path: '',
+        component: {
+          render: (h) => h('router-view'),
+        },
         children: [
           {
-            title: '刷卡情况分析',
-            img: 'float_absence@2x.png',
-            path: '/CreditCard',
-            component: () => import('@/views/WisdomReminder/PerceptualWisdom/CreditCard')
+            title: '违规查询',
+            icon: 'el-icon-menu',
+            img: 'left_ledger@2x.png',
+            name: 'ViolationEnquiry',
+            path: '/violation-enquiry',
+            component: () => import('@/views/IntegrityRisk/ViolationEnquiry'),
+          },
+          {
+            title: '检查通报',
+            icon: 'el-icon-menu',
+            img: 'float_inspection@2x.png',
+            name: 'InspectionBulletin',
+            path: '/bulletin',
+            component: () => import('@/views/WisdomReminder/InspectionBulletin'),
+          },
+          {
+            title: '涉嫌违法',
+            icon: 'el-icon-menu',
+            img: 'left_ledger@2x.png',
+            name: 'SuspectedViolation',
+            path: '/suspected',
+            component: () => import('@/views/IntegrityRisk/SuspectedViolation'),
           },
           {
             title: '提前就餐预警',
             img: 'float_absence@2x.png',
             path: '/EarlyMealWarning',
-            component: () => import('@/views/WisdomReminder/PerceptualWisdom/EarlyMealWarning')
-          }
-        ]
+            component: () => import('@/views/WisdomReminder/PerceptualWisdom/EarlyMealWarning'),
+          },
+        ],
       },
+
       {
-        title: '差旅管理',
+        title: '政务监督',
         icon: 'el-icon-menu',
-        img: 'float_travel@2x.png',
-        name: 'TravelManagement',
-        path: "/travel-management",
-        component: () => import('@/views/WisdomReminder/TravelManagement')
-      },
-      {
-        title: '护照管理',
-        icon: 'el-icon-menu',
-        img: 'float_travel@2x.png',
-        name: 'PassportManagement',
-        path: "/PassportManagement",
-        component: () => import('@/views/WisdomReminder/PassportManagement')
-      },
-      // 
-      {
-        title: '检查通报',
-        icon: 'el-icon-menu',
-        img: 'float_inspection@2x.png',
-        name: 'InspectionBulletin',
-        path: "/bulletin",
-        component: () => import('@/views/WisdomReminder/InspectionBulletin')
-      },
-      {
-        title: '违规查询',
-        icon: 'el-icon-menu',
-        img: 'left_ledger@2x.png',
-        name: 'WisdomAttendance',
-        path: "/violation-enquiry",
-        component: () => import('@/views/IntegrityRisk/ViolationEnquiry')
-      },
-      {
-        title: '护照管理',
-        icon: 'el-icon-menu',
-        img: 'left_ledger@2x.png',
-        name: 'WisdomAttendance',
-        path: "/passport",
-        component: () => import('@/views/IntegrityRisk/PassportManage')
-      },
-      {
-        title: '涉嫌违法',
-        icon: 'el-icon-menu',
-        img: 'left_ledger@2x.png',
-        name: 'WisdomAttendance',
-        path: "/suspected",
-        component: () => import('@/views/IntegrityRisk/SuspectedViolation')
-      },
-      {
-        title: '用车分析',
-        icon: 'el-icon-menu',
-        img: 'float_absence@2x.png',
-        name: 'AnalysisUse',
-        path: "analysis-use",
-        component: () => import('@/views/Supervision'),
+        img: 'float_attendance@2x.png',
+        path: '',
+        component: {
+          render: (h) => h('router-view'),
+        },
         children: [
+          {
+            title: '请销假分析',
+            icon: 'el-icon-menu',
+            img: 'float_absence@2x.png',
+            name: 'LeaveAnalysis',
+            path: '/leave-analysis',
+            component: () => import('@/views/Supervision'),
+            children: [
+             
+            ],
+          },
+        ],
+      },
+      {
+        title: '执法监督',
+        icon: 'el-icon-menu',
+        img: 'float_attendance@2x.png',
+        path: '',
+        component: { render: (h) => h('router-view'), },
+        children: [
+          {
+            title: '建设中',
+            icon: 'el-icon-menu',
+            img: 'float_attendance@2x.png',
+            name: 'build',
+            path: '/build',
+            component: {
+              render: (h) => h('router-view'),
+            },
+          },
+        ],
+      },
+      {
+        title: '警保监督',
+        icon: 'el-icon-menu',
+        img: 'float_attendance@2x.png',
+        path: '',
+        component: {
+          render: (h) => h('router-view'),
+        },
+        children: [
+          {
+            title: '差旅管理',
+            icon: 'el-icon-menu',
+            img: 'float_travel@2x.png',
+            name: 'TravelManagement',
+            path: '/travel-management',
+            component: () => import('@/views/WisdomReminder/TravelManagement'),
+          },
+          {
+            title: '刷卡情况分析',
+            img: 'float_absence@2x.png',
+            path: '/CreditCard',
+            component: () => import('@/views/WisdomReminder/PerceptualWisdom/CreditCard'),
+          },
+          {
+            title: '智慧感知',
+            icon: 'el-icon-menu',
+            img: 'left_sensing@2x.png',
+            name: 'PerceptualWisdom',
+            path: '/perceptual-wisdom',
+            component: () => import('@/views/Supervision'),
+            children: [],
+          },
           {
             title: '公车使用分析',
             img: 'float_car@2x.png',
@@ -153,19 +199,38 @@ const wisdomReminderRouter = [
             name: 'ViolationsCarManage',
             path: '/violation',
             component: () => import('@/views/WisdomReminder/ViolationsCarManage')
-          }
-        ]
+          },
+          {
+            title: '用车分析',
+            icon: 'el-icon-menu',
+            img: 'float_absence@2x.png',
+            name: 'AnalysisUse',
+            path: 'analysis-use',
+            component: () => import('@/views/Supervision'),
+          },
+        ],
       },
       {
-        title: '工作评价',
-        img: 'float_absence@2x.png',
+        title: '审计监督',
         icon: 'el-icon-menu',
-        name: 'EvaluationAnalysis',
-        path: '/evaluation-analysis',
-        component: () => import('@/views/WisdomReminder/EvaluationAnalysis')
-      }
-    ]
-  }
-]
+        img: 'float_attendance@2x.png',
+        path: '',
+        component: {
+          render: (h) => h('router-view'),
+        },
+        children: [
+          {
+            title: '建设中',
+            icon: 'el-icon-menu',
+            img: 'float_attendance@2x.png',
+            name: 'builda',
+            path: '/builda',
+            component: { render: (h) => h('router-view') }
+          },
+        ],
+      },
+    ],
+  },
+];
 
 export default wisdomReminderRouter;
